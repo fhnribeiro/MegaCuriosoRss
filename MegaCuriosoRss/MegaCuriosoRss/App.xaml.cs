094b8cc2a05dc5bfc7e324bb11a7d51a439a9a56
fhnribeiro <fhnribeiro@hotmail.com>
2020-11-02 23:19:23 -0300

    Adição suporte android 10

--- MegaCuriosoRss/MegaCuriosoRss/App.xaml.cs
+++ MegaCuriosoRss/MegaCuriosoRss/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using View.MegaCuriosoRss;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace MegaCuriosoRss
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
