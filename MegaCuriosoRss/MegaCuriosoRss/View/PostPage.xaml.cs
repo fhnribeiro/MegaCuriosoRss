94e293bf092d11d3aa5e8363137199cfdcc63987
fhnribeiro <fhnribeiro@hotmail.com>
2020-11-02 23:32:11 -0300

    Arquivos faltando

---
+++ MegaCuriosoRss/MegaCuriosoRss/View/PostPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using MegaCuriosoRss.Model;
+using Xamarin.Forms;
+
+namespace MegaCuriosoRss.View
+{
+    public partial class PostPage : ContentPage
+    {
+        public PostPage()
+        {
+            InitializeComponent();
+            Xamarin.Forms.PlatformConfiguration.iOSSpecific.Page.SetUseSafeArea(this, true);
+        }
+
+        public PostPage(Item item)
+        {
+            InitializeComponent();
+            Xamarin.Forms.PlatformConfiguration.iOSSpecific.Page.SetUseSafeArea(this, true);
+
+            webView.Source = item.ItemLink;
+        }
+    }
+}
